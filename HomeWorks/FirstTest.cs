d4bd40997e37845b0c179cf5605babb6764974d0
Kovbel Alexander <kovbels@gmail.com>
2016-11-18 09:34:11 +0200

    UpdateProject
    Create correct path and correct gitignor

---
+++ HomeWorks/FirstTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace SeleniumExample
+{
+    [TestFixture]
+    public class FirstTest
+    {
+        //ChromeDriver chrome;
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        [SetUp]
+
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+        }
+
+
+
+        [Test]
+        public void EasyTest()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Url = ("http://Google.com");
+            driver.FindElement(By.Name("q")).SendKeys("WebDriver");
+            driver.FindElement(By.Name("btnG")).Click();
+            wait.Until(ExpectedConditions.TitleIs("WebDriver - Google Search"));
+        }
+
+        // разрушение объекта драйвера после окончание теста.
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ HomeWorks/LoginAT.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace SeleniumExample
+{
+    [TestFixture]
+    public class LoginAT
+    {       
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        [SetUp]
+
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+        }
+
+        [Test]
+        public void CheckLoginAT()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Url = ("http://localhost/litecart/admin");
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+            wait.Until(ExpectedConditions.TitleIs("My Store"));
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
