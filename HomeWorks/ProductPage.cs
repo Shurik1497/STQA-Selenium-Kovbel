7a86906fbde936f932f9134f121cbdade91ba983
Kovbel Alexander <kovbels@gmail.com>
2016-12-01 00:47:38 +0200

    Add new test. CheckProductPage

---
+++ HomeWorks/ProductPage.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace HomeWorks
+{
+    [TestFixture]
+    public class ProductPage
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        [SetUp]
+
+        public void Start()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            driver = new ChromeDriver(options);
+            //driver = new InternetExplorerDriver();
+            //driver = new FirefoxDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(50));
+        }
+
+        [Test]
+        public void CheckProductPage()
+        {
+            string regularPriceExpectedClassMain = "regular-price";
+            string campaignPriceExpectedClassMain = "campaign-price";
+            string regularPriceExpectedClassProduct = "regular-price";
+            string campaignPriceExpectedClassProduct = "campaign-price";
+
+            driver.Url = ("http://localhost/litecart/en/");
+
+            IWebElement webElement = driver.FindElement(By.CssSelector("div#box-campaigns li:nth-of-type(1) a.link"));
+
+            ProductProperties propertiesMainPage = new ProductProperties(webElement);
+
+            webElement.Click();
+
+            wait.Until(ExpectedConditions.TitleContains(propertiesMainPage.productName));
+
+            webElement = driver.FindElement(By.CssSelector("div#box-product"));
+
+            ProductProperties propertiesProductPage = new ProductProperties(webElement);
+
+            //Check styles
+            if (propertiesMainPage.regularPriceeClass != regularPriceExpectedClassMain)
+                throw new System.Exception("Class of regular price on Main page isn't correct");
+            if (propertiesMainPage.campaignPriceClass != campaignPriceExpectedClassMain)
+                throw new System.Exception("Class of campaign price on Main page isn't correct");
+            if (propertiesProductPage.regularPriceeClass != regularPriceExpectedClassProduct)
+                throw new System.Exception("Class of regular price on Product page isn't correct");
+            if (propertiesProductPage.campaignPriceClass != campaignPriceExpectedClassProduct)
+                throw new System.Exception("Class of campaign price on Product page isn't correct");
+
+            //Check prices
+            if (propertiesMainPage.regularPricee != propertiesProductPage.regularPricee)
+                throw new System.Exception("Regular prise on Product page is different from regular prise on Main page");
+            if (propertiesMainPage.campaignPrice != propertiesProductPage.campaignPrice)
+                throw new System.Exception("Campaign prise on Product page is different from campaign prise on Main page");
+
+            //Check Titles
+            if (propertiesMainPage.productName != propertiesProductPage.productName)
+                throw new System.Exception("Title on Product page is different from title on Main page");
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ HomeWorks/ProductProperties.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HomeWorks
+{
+    class ProductProperties
+    {
+        public string productName { get; }
+        public string regularPricee { get; }
+        public string regularPriceeClass { get; }
+        public string campaignPrice { get; }
+        public string campaignPriceClass { get; }
+
+        public ProductProperties (IWebElement element)
+        {
+            Dictionary<string, string> productProperties = new Dictionary<string, string>();
+
+            try
+            {
+                productName = element.FindElement(By.CssSelector("h1.title")).GetAttribute("innerText");
+            }
+            catch (OpenQA.Selenium.NoSuchElementException)
+            {
+                productName = element.FindElement(By.CssSelector("div.name")).GetAttribute("innerText");
+            }
+
+            regularPricee = element.FindElement(By.CssSelector("div.price-wrapper s")).Text;
+            regularPriceeClass = element.FindElement(By.CssSelector("div.price-wrapper s")).GetAttribute("class");
+
+            campaignPrice = element.FindElement(By.CssSelector("div.price-wrapper strong")).Text;
+            campaignPriceClass = element.FindElement(By.CssSelector("div.price-wrapper strong")).GetAttribute("class");
+        }
+
+    }
+}
